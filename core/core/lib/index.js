baaf9bc4752dda616ab6d6db2f4c15a876daea83
liudongming <mrliudm@qq.com>
2022-05-21 15:52:02 +0800

    log package.json

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -3,12 +3,15 @@
 
 module.exports = core;
 
+const pkg = require('../package.json')
+
 function core() {
     // TODO
     console.log('exec core1')
+    checkPkgVersion()
 }
 
 
 function checkPkgVersion() {
-
+    console.log(pkg.version)
 }
