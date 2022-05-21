e43609c481e41e013fbca40d8d2e9496e7b4e4a5
liudongming <mrliudm@qq.com>
2022-05-21 15:34:29 +0800

    add import-local

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
 'use strict';
 
 module.exports = core;
 
 function core() {
     // TODO
+    console.log('exec core1')
 }
---
+++ utils/utils/lib/utils.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+'use strict';
+
+module.exports = log;
+
+function log() {
+    // TODO
+    console.log('utils');
+}
