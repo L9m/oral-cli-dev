9efff9237a8327bfd3e78f26ed2f616c9e81c430
L9m <aidenliudm@gmail.com>
2022-05-22 19:27:15 +0800

    add check version

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -3,11 +3,29 @@
 
 module.exports = core;
 
+const semver = require('semver')
+const colors = require('colors/safe')
+
 const pkg = require('../package.json')
 const log = require('@oral/log')
+const constant = require('./const')
 
 function core() {
-    checkPkgVersion()
+    try {
+        checkPkgVersion()
+    checkNodeVersion()
+    } catch(err) {
+        log.error(err.message)
+    }
+    
+}
+
+function checkNodeVersion() {
+    const currentVersion = process.version
+    const lowestVersion = constant.LOWEST_NODE_VERSION
+    if (!semver.gte(currentVersion, lowestVersion)) {
+        throw new Error(colors.red(`oral-cli 需要安装 v${lowestVersion} 以上版本的 Node.js`))
+    }
 }
 
 
