7f6dd879a7bef85c8ac00bb0fa32e6e2a618bfd9
L9m <aidenliudm@gmail.com>
2022-05-24 23:52:46 +0800

    add debug mode

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -5,16 +5,23 @@ module.exports = core;
 
 const semver = require('semver')
 const colors = require('colors/safe')
+const os = require('os')
+const pathExists = require('path-exists').sync
+
+let args
 
 const pkg = require('../package.json')
 const log = require('@oral/log')
-const constant = require('./const')
+const constant = require('./const');
 
 function core() {
     try {
         checkPkgVersion()
         checkNodeVersion()
         checkRoot()
+        checkUserHome()
+        checkInputArgs()
+        log.verbose('debug', 'test budeg log')
     } catch(err) {
         log.error(err.message)
     }
@@ -32,6 +39,28 @@ function checkNodeVersion() {
     if (!semver.gte(currentVersion, lowestVersion)) {
         throw new Error(colors.red(`oral-cli 需要安装 v${lowestVersion} 以上版本的 Node.js`))
     }
+}
+
+function checkUserHome() {
+    const userHome = os.homedir()
+    if (!userHome || !pathExists(userHome)) {
+        throw new Error(colors.red('当前登录用户主目录不存在'))
+    }
+}
+
+function checkInputArgs() {
+    const minimist = require('minimist')   
+    args = minimist(process.argv.slice(2))
+    checkArgs()
+}
+
+function checkArgs() {
+    if (args.debug) {
+        process.env.LOG_LEVEL = 'verbose'
+    } else {
+        process.env.LOG_LEVEL = 'info'
+    }
+    log.level = process.env.LOG_LEVEL
 }
 
 
